70758de6f67a5295ea078f8ace45bec977d09a3f
Jim Kelly <87jimkelly@gmail.com>
2017-01-14 15:21:28 +0000

    Activities CRUD

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,7 @@
 class ActivitiesController < ApplicationController
 
+	include ActivitiesHelper
+
 	def index
 		@activities = Activity.all
 	end
@@ -8,4 +10,32 @@ class ActivitiesController < ApplicationController
 		@activity = Activity.find(params[:id])
 	end
 
+	def new
+		@activity = Activity.new
+	end
+
+	def create
+		@activity = Activity.new(activity_params)
+		@activity.save
+		flash.notice = "Activity '#{@activity.title}' created"
+		redirect_to activity_path(@activity)
+	end
+
+	def destroy
+		@activity = Activity.find(params[:id])
+		@activity.destroy
+		flash.notice = "Activity '#{@activity.title}' deleted"
+		redirect_to activities_path
+	end
+
+	def edit
+		@activity = Activity.find(params[:id])
+	end
+
+	def update
+		@activity = Activity.find(params[:id])
+		@activity.update(activity_params)
+		flash.notice = "Activity '#{@activity.title}' updated"
+		redirect_to activity_path(@activity)
+	end
 end
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,5 @@
 module ActivitiesHelper
+	def activity_params
+		params.require(:activity).permit(:title, :location)
+	end
 end
