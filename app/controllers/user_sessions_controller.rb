4bdc9bd584ab4f2187ea82dfc90633b0164143e3
Jim Kelly <87jimkelly@gmail.com>
2017-06-30 21:40:17 +0100

    turned off autocapitalization on login form, force new users to change password

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,9 +4,21 @@ class UserSessionsController < ApplicationController
 	def new
 	end
 
+	#Login
 	def create
 		if login(params[:username], params[:password])
-			redirect_back_or_to(activities_path, notice: "Logged in successfully")
+			# Save user into variable
+			@user = current_user
+
+			#Check if user password is "password"
+			if @user.valid_password?('password')
+				#if it is, ask them to change it
+				redirect_to(edit_user_path(@user))
+				flash.alert = "You need to change your password"
+			else
+				#else log them in and take them to activities.
+				redirect_back_or_to(activities_path, notice: "Logged in successfully")
+			end
 		else
 			flash.now.alert = "Login Failed"
 			render action: :new
